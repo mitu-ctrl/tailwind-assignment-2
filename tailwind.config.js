edc315c170536f57f8b5e109e441d9503644fdca
mitu-ctrl <moriomakter979@gmail.com>
2024-09-30 17:38:13 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+
+      colors: {
+        'orange':'#F44335'
+        
+        },
+       
+    },
+  },
+  plugins: [],
+}
+
